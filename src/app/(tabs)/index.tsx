2f2c9c6fa5f0859e7790e5ddc924f67a40775897
Ezea Victor Chukwuebuka <53367180+Victorvikson1996@users.noreply.github.com>
2024-02-26 05:41:26 +0100

    Styles

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,7 +1,5 @@
-import { StyleSheet, FlatList } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
+import { StyleSheet, FlatList, View } from 'react-native';
+import { Text } from '@/src/components/Themed';
 import { Stack } from 'expo-router';
 import top5 from '@/assets/data/top5.json';
 import { StockListItem } from '@/src/components/StockListItem';
@@ -15,6 +13,7 @@ export default function TabOneScreen() {
       <FlatList
         data={stocks}
         renderItem={({ item }) => <StockListItem stock={item} />}
+        contentContainerStyle={styles.content}
       />
     </View>
   );
@@ -25,5 +24,9 @@ const styles = StyleSheet.create({
     flex: 1
     // alignItems: 'center',
     // justifyContent: 'center'
+  },
+  content: {
+    gap: 20,
+    padding: 10
   }
 });
--- src/components/StockListItem.tsx
+++ src/components/StockListItem.tsx
@@ -1,6 +1,8 @@
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import React from 'react';
 import { Text } from './Themed';
+import Colors from '../constants/Colors';
+import { AntDesign } from '@expo/vector-icons';
 
 type Stock = {
   name: String;
@@ -15,8 +17,27 @@ type StockListItem = {
 
 export const StockListItem = ({ stock }: StockListItem) => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Text style={styles.symbol}>
+        {stock.symbol} <AntDesign name='staro' size={18} color='gray' />
+      </Text>
+      <Text style={styles.name}>{stock.name}</Text>
       <Text>{stock.close}</Text>
+      <Text>{stock.percent_change}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  symbol: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: Colors.light.tint
+  },
+  name: {
+    color: 'gray'
+  }
+});
